b7e09d02e101f4fb9c994e2c01cb4bc888b5db2e
schaldach <gabimorgado0311@gmail.com>
2022-10-01 14:35:34 -0300

    animação para o clique no botao de add jogadores
    keyframes e animação para abrir e fechar

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ function changeState(documentID){
     let element = document.getElementById(documentID);
     if(element.classList.contains(documentID + '--open')){
         element.classList.remove(documentID + '--open')
+        element.classList.add(documentID + '--closed')
     }else{
         element.classList.add(documentID + '--open')
+        element.classList.remove(documentID + '--closed')
     }
 }
