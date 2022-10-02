9dab1b77e364adf8bc196b0388744328c6d95d0b
FIRST_NAME LAST_NAME <62450294+Lucsvieirabr@users.noreply.github.com>
2022-10-01 21:59:16 -0300

    Arrumando a merda histórica

--- main.js
+++ main.js
@@ -2,24 +2,7 @@ function buttoncliked(){
     changeState('nav')
     let playersBox = document.getElementById('players');
     let pdisplay = window.getComputedStyle(playersBox).display;
-<<<<<<< HEAD
-    if(pdisplay == 'none'){
-        playersBox.classList.add('players' + '--open')
-    }else{
-        playersBox.classList.remove('players')
-        playersBox.classList.add('players' + '--closed')
-        setTimeout(()=>{
-            playersBox.classList.remove('players' + '--closed')
-            playersBox.classList.remove('players' + '--open')
-            playersBox.classList.add('players')
-
-        }, 4000)
-    }
-
-
-=======
     playersBox.style.display = pdisplay == 'none' ? 'grid' : 'none'
->>>>>>> 8a40a9f80f20d58bdc4d77c89493e33411dd7086
 }
 
 function changeState(documentID){
