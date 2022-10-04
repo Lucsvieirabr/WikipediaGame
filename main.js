9218a7edf5e74b801cac2baecc8b126f8a6a297d
FIRST_NAME LAST_NAME <62450294+Lucsvieirabr@users.noreply.github.com>
2022-10-03 21:08:59 -0300

    Smooth White Box closing Anima
    ISQUEÇAAAA
    :fire: :fire:

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
+let isAnimating = false;
 function buttoncliked(){
-    changeState('nav')
-    let playersBox = document.getElementById('players');
-    let pdisplay = window.getComputedStyle(playersBox).display;
-    playersBox.style.display = pdisplay == 'none' ? 'grid' : 'none'
+    if(!isAnimating){
+        changeState('nav')
+        whiteBoxAnimationController('players')
+    }
 }
-
+function whiteBoxAnimationController(docId){
+    let playersBox = document.getElementById(docId);
+    if(playersBox.classList.contains(docId + '--open') && !isAnimating){
+        isAnimating = true
+        playersBox.classList.remove(docId)
+        playersBox.classList.add(docId + '--closed')
+        setTimeout(()=>{
+            playersBox.classList.remove(docId + '--open')
+            playersBox.classList.remove(docId + '--closed')
+            playersBox.classList.add(docId)
+            isAnimating = false;
+        },2600)
+        
+    }else{
+            playersBox.classList.add(docId + '--open')
+        }
+}
 function changeState(documentID){
     let element = document.getElementById(documentID);
     if(element.classList.contains(documentID + '--open')){
