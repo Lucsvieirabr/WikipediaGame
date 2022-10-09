4572b312709fab69d9e141a34eec54e83dbf2718
FIRST_NAME LAST_NAME <62450294+Lucsvieirabr@users.noreply.github.com>
2022-10-09 15:40:45 -0300

    Confetti Animation

--- main.js
+++ main.js
@@ -35,16 +35,52 @@ function createPlayer(){
     profilePic.style.background =`linear-gradient(to right, ${mygradient[0]}, ${mygradient[1]})`
     gradientBgPlayer.appendChild(profilePic)
     footer.classList.add('footer')
+    let confettiDiv = document.createElement('div')
+    confettiDiv.classList.add('svg')
+    confettiDiv.setAttribute('id', 'svg' + (getNumberOfPlayers()+ 1))
+    div.appendChild(confettiDiv)
     div.appendChild(gradientBgPlayer)
     div.appendChild(footer)
     return div
 }
 function addPlayer(){
+    let divIsEmpety = checkPlayersDiv()
     let playerCard = createPlayer();
     let playerDiv = document.createElement('div')
     playerDiv.classList.add('player')
     playerDiv.appendChild(playerCard);
+    if(divIsEmpety){
+        setTimeout(()=>{
+            animationplayerAndPutOnDiv(playerDiv)
+        },2200)
+        return
+    }
+    animationplayerAndPutOnDiv(playerDiv)
+}
+
+function getNumberOfPlayers(){
+    var element = document.getElementById("players");
+    var numberOfChildren = element.childElementCount
+    return numberOfChildren
+}
+function animationplayerAndPutOnDiv(playerDiv){
     document.getElementById('players').appendChild(playerDiv)
+    console.log(getNumberOfPlayers())
+
+    let svgContainer = document.getElementById('svg' + getNumberOfPlayers());
+    const animItem = bodymovin.loadAnimation({
+            wrapper: svgContainer,
+            animType: 'svg',
+            loop: false,
+            autoplay: false,
+            path: 'https://assets2.lottiefiles.com/packages/lf20_u4yrau.json'
+        });
+    animItem.goToAndPlay(0,true);
+    
+    animItem.addEventListener('complete', () => {
+        svgContainer.remove();
+        
+    })
 }
 
 function checkPlayersDiv(){
